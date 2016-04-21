de0ec606ae53353eb3a56a74059d4d16e7788e2d
jansanchez <joejansanchez@gmail.com>
2016-04-21 16:49:00 -0500

    docs(readme): agrego documentacion

---
+++ readme.md
@@ -0,0 +1,44 @@
+# ramelix
+A RAML configuration demo + osprey
+
+## Install 
+
+```
+npm install -d
+```
+
+## How to use
+
+## Use it from `npm scripts`
+
+### Generate html 
+
+```
+npm run html
+```
+
+### Start API REST Server Dummy by port 3000
+
+```
+npm start
+```
+
+```
+http://localhost:3000/
+```
+
+### Executing server for documentation by port 8000
+
+```
+npm run server
+```
+
+```
+http://localhost:8000/
+```
+
+### Executing js linter
+
+```
+npm run lint
+```
--- routes/raml.js
+++ routes/raml.js
@@ -1,9 +1,39 @@
-var osprey = require('osprey');
-var router = osprey.Router();
+#!/usr/bin/env node
+'use strict';
 
-router.get('/token', function(req, res) {
-    res.json({"token": "1234556"});
-});
+const fs = require('fs');
+const osprey = require('osprey');
+const color = require('chalk');
+const router = osprey.Router();
+
+const callback = (request, response) => {
+	const charset = 'utf-8';
+	const name = request.url.replace(/^\//, "");
+	const jsonFile = `api/examples/${name}.response.json`;
+	console.log(color.magenta('URL Name: ') + color.bold(name));
+	console.log(color.cyan('Request:'));
+	console.log(color.gray('Body:'));
+	console.log(request.body);// logs { deviceId: 'deviceid' }
+	try {
+		fs.readFile(jsonFile, charset, (err, data) => {
+			if (err) {
+				throw err;
+			}
+			response.setHeader('Content-Type', `application/json; charset=${charset}`);
+			response.end(data);
+			console.log(color.cyan('Response:'));
+			console.log(data);
+			console.log(color.gray('.').repeat(45));
+			console.log('');
+		});
+	}	catch	(e)	{
+		console.log(e);
+	}
+};
+
+/*
+* Routers
+*/
+router.post('/token', callback);
 
 module.exports = router;
-
